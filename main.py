50fb57d1103b6c37074770a62b5f131998d27105
Farhan <39709841+fgkhan@users.noreply.github.com>
2023-12-06 19:20:53 -0500

    Leearning class

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from user import User
-from post import Post
+import user
+import post
 
 
-app_user_one = User("fgkhan@hotmail.com", "Nana", "pwd1", "DevOps Engineer")
+app_user_one = user.User("fgkhan@hotmail.com", "Nana", "pwd1", "DevOps Engineer")
 app_user_one.get_user_info()
 app_user_one.change_job_title("manager")
 app_user_one.get_user_info()
 
-new_post = Post('Test', app_user_one.name)
+new_post = post.Post("teset", app_user_one.name)
 new_post.get_post_info()
--- post.py
+++ post.py
@@ -1,7 +1,7 @@
 class Post:
-    def __int__(self, message, author):
-        self.message = message
-        self.author = author
+    def __int__(self, post_message, post_author):
+        self.message = post_message
+        self.author = post_author
 
     def get_post_info(self):
         print(f"Post: {self.message} written by {self.author}")
